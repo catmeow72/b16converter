3d03ea6e27b4b806b371ed43c0df415ef7a04558
Zachary Hall <catmeow72@proton.me>
2023-12-01 16:03:56 -0800

    Fix compat layer.

--- win32compat.h
+++ win32compat.h
@@ -2,7 +2,7 @@
 
 #ifdef _WIN32
 #include <stdlib.h>
-inline void setenv(char const *variable, char const *value, bool replace) {
+inline void setenv(char const *variable, char const *value, char replace) {
 	if (replace || getenv(variable) == NULL || getenv(variable) == "") {
 		if (value == NULL) {
 			_putenv_s(variable, "");
